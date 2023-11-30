553dde983193cf0335bbaa14f403c8322836b32e
Клим Коноплёв <melisakon45@gmail.com>
2023-11-30 12:20:58 +0300

    feat: Добавлен норм вывод, но нихера больше 1000 не работает (?)

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 using namespace chrono;
 
-const int n = 15;
+const int n = 1000;
 
 // Функция для обмена двух элементов массива
 void swap(int& a, int& b) {
@@ -18,45 +18,41 @@ void swap(int& a, int& b) {
 }
 
 // Generating an increasing array
-void UpToLowArray(int* array, int size, int minValue, int maxValue) {
-    if (size >= (maxValue - minValue + 1)) {
-        for (int i = 0; i < size; i++) {
-            array[i] = maxValue;
-            if (minValue < maxValue)
-                maxValue--;
+void LowToUpArray(int* array, int size, int minValue, int maxValue) {
+    int i = 0;
+    int k = minValue;
+    while (i < size) {
+        array[i] = k;
+        if (k < maxValue) {
+            k++;
         }
-    }
-    else {
-        int left = maxValue - size + 1;
-        int right = maxValue;
-        for (int i = 0; i < size; i++) {
-            array[i] = (double)rand() / RAND_MAX * (right - left) + left;
-            right = array[i] - 1;
-            if (left > size)
-                left = right - size + 1;
-            else
-                left--;
+        else {
+            k = maxValue;
         }
+        i++;
     }
 }
 
 // Generating a descending array
-void LowToUpArray(int* array, int size, int minValue, int maxValue) {
-    if (size >= (maxValue - minValue + 1)) {
-        for (int i = 0; i < size; i++) {
-            array[i] = minValue;
-            if (minValue < maxValue)
-                minValue++;
+void UpToLowArray(int* array, int size, int minValue, int maxValue) {
+    int i = 0;
+    int k = maxValue;
+    while (i < size) {
+        array[i] = k;
+        if (k > minValue) {
+            k--;
         }
+        else {
+            k = minValue;
+        }
+        i++;
     }
-    else {
-        int left = minValue;
-        int right = maxValue - size + 1;
-        for (int i = 0; i < size; i++) {
-            array[i] = (double)rand() / RAND_MAX * (right - left) + left;
-            left = array[i] + 1;
-            right++;
-        }
+}
+
+// Generating a random array
+void RandomArray(int* array, int size, int minValue, int maxValue) {
+    for (int i = 0; i < size; i++) {
+        array[i] = (double)rand() / RAND_MAX * (maxValue - minValue) + minValue;
     }
 }
 
@@ -74,55 +70,25 @@ vector<long int> selectionSort(int array[], int n, ofstream& outputFile) {
             }
         }
         // Обмен текущего элемента с минимальным
-        swap(array[i], array[minIndex]);
-        swaps += 1;
+        if (i != minIndex) {
+            swap(array[i], array[minIndex]);
+            swaps += 1;
+        }
 
         // Вывод массива после каждой итерации в файл
-        if (n <= 15) {
+ /*       if (n <= 14) {
             outputFile << "Selection Sort Iteration " << i + 1 << ": ";
             for (int k = 0; k < n; ++k) {
                 outputFile << array[k] << " ";
             }
             outputFile << endl;
-        }
+        }*/
     }
     params.push_back(swaps);
     params.push_back(comp);
 
     return params;
 }
-
-vector<long int> selectionSort_Down(int array[], int n, ofstream& outputFile) {
-    vector<long int> params;
-    int swaps = 0;
-    long int comp = 0;
-    for (int i = 0; i < n; ++i) {
-        int maxIndex = i;
-        for (int j = i + 1; j < n; ++j) {
-            comp++;
-            if (array[j] > array[maxIndex]) {
-                maxIndex = j;
-            }
-        }
-        // Обмен текущего элемента с минимальным
-        swap(array[i], array[maxIndex]);
-        swaps += 1;
-
-        // Вывод массива после каждой итерации в файл
-        if (n <= 15) {
-            outputFile << "Selection Sort Iteration " << i + 1 << ": ";
-            for (int k = 0; k < n; ++k) {
-                outputFile << array[k] << " ";
-            }
-            outputFile << endl;
-        }
-    }
-    params.push_back(swaps);
-    params.push_back(comp);
-
-    return params;
-}
-
 
 // Быстрая сортировка
 vector<long int> quickSort(int array[], int low, int high, ofstream& outputFile) {
@@ -139,24 +105,28 @@ vector<long int> quickSort(int array[], int low, int high, ofstream& outputFile)
         // Разделение массива на две части: элементы, меньшие divider, и элементы, большие divider
         for (int j = low; j < high; ++j) {
             comp++;
-            if (array[j] < divider) {
+            if (array[j] <= divider) {
                 ++i;
-                swap(array[i], array[j]);
-                swaps += 1;
+                if (i != j) {
+                    swap(array[i], array[j]);
+                    swaps += 1;
+                }
             }
         }
         // Обмен опорного элемента с элементом, стоящим следующим после меньших элементов
-        swap(array[i + 1], array[high]);
-        swaps += 1;
+        if (i + 1 != high) {
+            swap(array[i + 1], array[high]);
+            swaps += 1;
+        }
 
         // Вывод массива после каждой итерации в файл
-        if (n <= 15) {
+      /*  if (sizeof(array) / 4 <= 14) {
             outputFile << "Quick Sort Iteration: ";
             for (int k = low; k <= high; ++k) {
                 outputFile << array[k] << " ";
             }
             outputFile << endl;
-        }
+        }*/
 
         int partitionIndex = i + 1;
 
@@ -183,62 +153,126 @@ vector<long int> quickSort(int array[], int low, int high, ofstream& outputFile)
     return params;
 }
 
+
 int main() {
     srand(time(0));
 
     int array[n];
-
+    
     // Генерация и вывод исходного массива
-    cout << "Original Array: ";
     ofstream originalFile("original.txt");
     originalFile << "Original Array: ";
+    LowToUpArray(array, n, 0, 100);
     for (int i = 0; i < n; ++i) {
-        array[i] = rand() % 100;  // Заполняем массив случайными числами от 0 до 99
-        cout << array[i] << " ";
         originalFile << array[i] << " ";
     }
-    cout << endl;
     originalFile.close();
+    ofstream file("data.txt");
 
     // Измерение времени для сортировки выбором
-    auto startSelectionSort = high_resolution_clock::now();
     int arraySelection[n];
     copy(begin(array), end(array), begin(arraySelection));
-    ofstream selectionFile("selection_sort.txt");
-    vector<long int> selectionParams = selectionSort_Down(arraySelection, n, selectionFile);
+    auto startSelectionSort = high_resolution_clock::now();
+    vector<long int> selectionParams = selectionSort(arraySelection, n, file);
     auto stopSelectionSort = high_resolution_clock::now();
     auto durationSelectionSort = duration_cast<microseconds>(stopSelectionSort - startSelectionSort);
 
     // Вывод отсортированного массива и времени выполнения сортировки выбором
-    selectionFile << "Selection Sort Result: ";
-    for (int i = 0; i < n; ++i) {
-        selectionFile << arraySelection[i] << " ";
-    }
-    selectionFile << endl;
-    selectionFile << "Time taken by Selection Sort: " << durationSelectionSort.count() << " microseconds" << endl;
-    selectionFile << "Number of Swaps (Selection Sort): " << selectionParams[0] << endl;
-    selectionFile << "Number of Comparisons (Selection Sort): " << selectionParams[1] << endl;
-    selectionFile.close();
+    //selectionFile << "Selection Sort Result: ";
+    //for (int i = 0; i < n; ++i) {
+    //    selectionFile << arraySelection[i] << " ";
+    //}
+    //selectionFile << endl;
+    file << "Time taken by Selection Sort: " << durationSelectionSort.count() << " microseconds" << endl;
+    file << "Number of Swaps (Selection Sort): " << selectionParams[0] << endl;
+    file << "Number of Comparisons (Selection Sort): " << selectionParams[1] << endl;
 
     // Измерение времени для быстрой сортировки
-    auto startQuickSort = high_resolution_clock::now();
     int arrayQuick[n];
     copy(begin(array), end(array), begin(arrayQuick));
-    ofstream quickFile("quickSort.txt");
-    vector<long int> quickParams = quickSort(arrayQuick, 0, n - 1, quickFile);
+    auto startQuickSort = high_resolution_clock::now();
+    vector<long int> quickParams = quickSort(arrayQuick, 0, n - 1, file);
     auto stopQuickSort = high_resolution_clock::now();
     auto durationQuickSort = duration_cast<microseconds>(stopQuickSort - startQuickSort);
 
     // Вывод отсортированного массива и времени выполнения быстрой сортировки
-    quickFile << "Quick Sort Result: ";
-    for (int i = 0; i < n; ++i) {
-        quickFile << arrayQuick[i] << " ";
-    }
-    quickFile << endl;
-    quickFile << "Time taken by Quick Sort: " << durationQuickSort.count() << " microseconds" << endl;
-    quickFile << "Number of Swaps (Quick Sort): " << quickParams[0] << endl;
-    quickFile << "Number of Comparisons (Quick Sort): " << quickParams[1] << endl;
-    quickFile.close();
+    //quickFile << "Quick Sort Result: ";
+    //for (int i = 0; i < n; ++i) {
+    //    quickFile << arrayQuick[i] << " ";
+    //}
+    //quickFile << endl;
+    file << "Time taken by Quick Sort: " << durationQuickSort.count() << " microseconds" << endl;
+    file << "Number of Swaps (Quick Sort): " << quickParams[0] << endl;
+    file << "Number of Comparisons (Quick Sort): " << quickParams[1] << endl;
+    file.close();
 
     return 0;
 }
+
+//int main() {
+//    srand(time(0));
+//
+//    void(*genArr[])(int*, int, int, int) = { LowToUpArray, UpToLowArray, RandomArray };
+//    ofstream file("data.txt");  
+//
+//    steady_clock::time_point startTimeSelection;
+//    steady_clock::time_point stopTimeSelection;
+//    microseconds durationTimeSelection;
+//
+//    steady_clock::time_point startTimeQuick;
+//    steady_clock::time_point stopTimeQuick;
+//    microseconds durationTimeQuick;
+//
+//    file << "Array Type\t\tTime\t\t\tSwaps\t\tComparations" << endl;
+//    file << "\t\t\tSelect\tQuick\tSelect\tQuick\tSelect\tQuick\t" << endl;
+//
+//    for (int i = 0; i < 3; i++) {
+//        int array[n];
+//
+//        // Array generating
+//        genArr[i](array, n, 0, 100);
+//
+//        // Измерение времени для сортировки выбором
+//        int arraySelection[n];
+//        int arrayQuick[n];
+//        copy(begin(array), end(array), begin(arraySelection));
+//        startTimeSelection = high_resolution_clock::now();
+//        vector<long int> selectionParams = selectionSort(arraySelection, n, file);
+//        stopTimeSelection = high_resolution_clock::now();
+//        durationTimeSelection = duration_cast<microseconds>(stopTimeSelection - startTimeSelection);
+//
+//
+//        // Измерение времени для сортировки выбором
+//        copy(begin(array), end(array), begin(arrayQuick));
+//        startTimeQuick = high_resolution_clock::now();
+//        vector<long int> quickParams = quickSort(arrayQuick, 0, n - 1, file);
+//        stopTimeQuick = high_resolution_clock::now();
+//        durationTimeQuick = duration_cast<microseconds>(stopTimeQuick - startTimeQuick);
+//
+//        switch (i)
+//        {
+//        case 0:
+//            file << "Icrease\t\t";
+//            break;
+//        case 1:
+//            file << "Decrease\t";
+//            break;
+//        case 2:
+//            file << "Random\t\t";
+//            break;
+//        }
+//
+//        file << durationTimeSelection.count();
+//        file << "\t\t" << durationTimeQuick.count();
+//        file << "\t\t" << selectionParams[0];
+//        file << "\t\t" << quickParams[0];
+//        file << "\t\t" << selectionParams[1];
+//        file << "\t\t" << quickParams[1] << endl;
+//
+//        selectionParams.clear();
+//        quickParams.clear();
+//    }
+//    file.close();
+//
+//    return 0;
+//}
